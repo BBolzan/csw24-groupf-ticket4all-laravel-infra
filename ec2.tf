21c3f7e15e8c2fe65fd85e101009364d8d4e651d
BBolzan <bruno.sa@edu.pucrs.br>
2024-11-19 18:51:22 -0300

    adicionando porta publica

--- ec2.tf
+++ ec2.tf
@@ -13,11 +13,12 @@ resource "aws_instance" "instance_ec2" {
   instance_type = "t2.micro"
   key_name      = aws_key_pair.my_new_key_pair.key_name
   subnet_id     = aws_subnet.public_a.id
+  associate_public_ip_address = true
 
   vpc_security_group_ids = [aws_security_group.new_api_access.id]
 
   tags = {
-    Name = "new-instance-ec2"
+    Name = "new-ec2"
   }
 
   user_data = <<-EOF
